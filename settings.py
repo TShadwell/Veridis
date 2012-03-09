9f6b294401dd2900ad61d6dcb8d8be47c850720e
Thomas Shadwell <thomas.shadwell@googlemail.com>
2012-03-09 12:25:21 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 store ={}
-def readsettings(Notify):
+def readsettings(Notify, Veridis):
 	try:
 		tasks = open("data/tasks")
 		t=tasks.read().rstrip("}").split("\n")
@@ -10,4 +10,6 @@ def readsettings(Notify):
 		t = [dict([[(Aliases[a] if a in Aliases else a) for a in list(value.split(":"))] for value in task.split(";")]) for task in "".join([val for val in [(addAlias(line.split(" ")[1:3]) if not line.find("Alias") else line) for line in t] if not val == None]).split("}")]
 		return t
 	except IOError as e:
-		Notify.Queue.append("Could not load tasks!")
+		Notify.error("Could not read tasks!")
+		Veridis.running = False
+		return False
