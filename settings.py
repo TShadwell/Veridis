d07956c130e71d946afd807c8ddb12cc536e3ea5
Thomas Shadwell <thomas.shadwell@googlemail.com>
2012-03-09 12:31:40 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 tasks={}
 schedule={}
-def readsettings(filename, Notify, Veridis):
+from notify import error
+def readsettings(filename, Veridis):
 	try:
 		tasks = open(filename)
 		t=tasks.read().rstrip("}").split("\n")
@@ -11,6 +12,6 @@ def readsettings(filename, Notify, Veridis):
 		t = [dict([[(Aliases[a] if a in Aliases else a) for a in list(value.split(":"))] for value in task.split(";")]) for task in "".join([val for val in [(addAlias(line.split(" ")[1:3]) if not line.find("Alias") else line) for line in t] if not val == None]).split("}")]
 		return t
 	except IOError as e:
-		Notify.error("Could not read tasks!")
+		error("Could not read %s, aborting." % (filename))
 		Veridis.running = False
 		return False
