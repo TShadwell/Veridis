57cfb856ad82a4f1bed14530245544e85fc7899e
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2012-03-11 00:15:12 +0000

    ready steady gogo

--- init.py
+++ init.py
@@ -9,15 +9,38 @@ class Veridis:
 				Self.height=percheight
 				Self.left=percleft
 				Self.top=perctop
-				r_width=Self.width/100
-				r_height=Self.height/100
+				Self.r_width=Self.width/100
+				Self.r_height=Self.height/100
+		class table:
+			def __init__(Self, table, percx, percy,coloura, colourb, padding=(0,0), wrap=1):
+				Self.table=table
+				Self.x=percx
+				Self.y=percy
+				Self.padding=padding
+				Self.wrap=wrap
+			
+		class textobject:
+			def __init__(Self, text, percx, percy, padding=(0,0),attrs=0, wrap=-1):
+				Self.text= text
+				Self.top= percy
+				Self.left= percx
+				Self.r_top=Self.top/100
+				Self.r_left=Self.left/100
+				Self.wrap=wrap
+				Self.padding=padding
+				Self.attrs = attrs
+			def draw(Self, Screen):
+				height, width = Screen.getmaxyx()
+				for n, line in enumerate(Self.text.split("\n")):
+					Screen.addstr(n+int(Self.r_top*height)+Self.padding[1],int(Self.r_left*width)+Self.padding[0],line, Self.attrs)
 	def __init__(Self):
 		import notify
 		import settings
 		Self.notify=notify
 		Self.settings=settings
 	running = True
-	frames = {}
+	frames={}
+	Drawables={}
 #####INIT#####
 def init(Screen):
 	from main import main
--- main.py
+++ main.py
@@ -10,7 +10,23 @@ def tick(Screen, Veridis):
 	tick(Screen, Veridis)
 def clock(Screen, Veridis):
 	#Calculate the size of the upper-right box to see if an art-based clock would work.
-	pass
+	height, width = Screen.getmaxyx()
+	now=datetime.now().strftime("%H:%M")
+	if (int(width*0.3)-1) <= ((7 * len(now))):
+		#Display a text-based clock
+		Veridis.Drawables["clock"].text= now
+	else:
+		lines ={}
+		for char in now:
+			for n, line in enumerate(Veridis.settings.clockface[char].split("\n")):
+				if n in lines:
+					lines[n] += line.replace("#", "█")
+				else:
+					lines[n] = line.replace("#", "█")
+		Veridis.Drawables["clock"].text=("\n".join([text.strip(" ").replace("b", " ") for key, text in lines.items() if not len(text.strip(" "))==0]))
+	#notify.error(Veridis.Drawables["clock"].text)
+	update(Screen, Veridis)
+	Screen.refresh()
 def update(Screen, Veridis):
 	#Redraw frames
 	Screen.clear()
@@ -35,19 +51,32 @@ def update(Screen, Veridis):
 		#Draw in the pipes
 		Screen.addch(0,int(width*(x/100)), curses.ACS_BSSS)
 		Screen.addch(height-1,int(width*(x/100)), curses.ACS_BTEE)
+	#Calculate the padding for the clock
+	Veridis.Drawables["clock"].padding=(int(((width*0.3)/2)-(len(Veridis.Drawables["clock"].text.split("\n")[0])/2)),1)
+	#Draw in the textobjects
+	for name, t in Veridis.Drawables.items():
+		t.draw(Screen)
 	Screen.refresh()
 def react(input, Screen, Veridis):
 	if (input == 113):
 		Veridis.running=False
 	elif (input == 410):
 		update(Screen, Veridis)
+		clock(Screen, Veridis)
 #####MAIN#####
 def main(Screen, Veridis):
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	_thread.start_new_thread(tick,(Screen, Veridis))
 	#Make the 3 Main frames.
 	Veridis.frames["A"] = Veridis.types.frame(70,100,0,0)
 	Veridis.frames["B"] = Veridis.types.frame(10,60,70,0)
 	Veridis.frames["C"] = Veridis.types.frame(10,40,70,60)
+	#Draw in titles
+	Veridis.Drawables["now"] = Veridis.types.textobject("Now", 70,0, (1,0))
+	Veridis.Drawables["next"] = Veridis.types.textobject("Next", 70,60, (1,0))
+	#Clock placeholder
+	Veridis.Drawables["clock"] = Veridis.types.textobject("clock", 70,0, (1,1), curses.color_pair(1))
+	Veridis.Drawables["timetable"] = Veridis.types.textobject("\n".join([str(x)for x in range(Screen.getmaxyx()[0]-10)]),0,0,(1,1))
 	while Veridis.running:
 		Screen.refresh()
 		update(Screen, Veridis)
